1c058f9eeac2b4805ef95b9b338be5818b6cb2f4
josehenriques10 <josehenriques10@outlook.com>
2024-01-21 13:51:34 -0300

    Código javaScript refatorado.

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,34 +1,30 @@
-(function () {
-    "use strict";
+"use strict";
 
-    const form = document.querySelector('form');
+(function () {
+    const form = document.forms.namedItem('form');
     const resultado = document.querySelector('#resultado');
     
     form.addEventListener('submit', event => {
         event.preventDefault();
 
-        const valores = [
-            document.querySelector('#mes').value,
-            document.querySelector('#diasLetivos').value,
-            document.querySelector('#faltas').value
+        const [ mes, diasLetivos, faltas ] = [
+            form.mes.value, 
+            form.diasLetivos.value, 
+            form.faltas.value
         ];
 
-        const frequencia = (valores[1] - valores[2]) * 100/valores[1];
-
+        const frequencia = (diasLetivos - faltas) * 100 / diasLetivos;
+        
         if (event) {
             resultado.style.display = 'block';
 
-            const porcentagemFrequencia = document.querySelector('#porcentagemFrequencia');
-            porcentagemFrequencia.innerText = `${frequencia.toFixed(1)}%`;
+            document.querySelector('#porcentagemFrequencia').innerText = `${frequencia.toFixed(1)}%` ;
 
-            const resultMes = document.querySelector('#resultMes');
-            resultMes.innerText = `${valores[0].toUpperCase()} de 2024`;
+            document.querySelector('#resultMes').innerText = `${mes.toUpperCase()} de 2024`;
 
-            const resultDiasLet = document.querySelector('#resultDiasLet');
-            resultDiasLet.innerText = `${valores[1]} dias letivos`;
+            document.querySelector('#resultDiasLet').innerText = `${diasLetivos} letivos`;
 
-            const resultFaltas = document.querySelector('#resultFaltas');
-            resultFaltas.innerText = resultFaltas.innerText = `${valores[2]} faltas`
+            document.querySelector('#resultFaltas').innerText = `${faltas} faltas`;
         }
     });
 })();
